2f23658b8c9fbc5c345e588651ecb9573f26f073
emanuel <almemanuel>
2022-03-08 09:32:14 -0300

    decode

--- index.php
+++ index.php
@@ -11,20 +11,19 @@
 </head>
 <body>
     <?php
-        function responseCode($url){
-            $ch = curl_init($url);
-            curl_setopt($ch, CURLOPT_HEADER, true);    // we want headers
-            curl_setopt($ch, CURLOPT_NOBODY, true);    // we don't need body
-            $output = curl_exec($ch);
-            $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-            curl_close($ch);
-            return $httpcode;
-        }
-
         function getPokemon($url){
-            if(responseCode($url) == 200) {
-                return json_decode(file_get_contents($url));
-            } return ["!200" => "ERROR"];
+            $curl = curl_init($url);
+            curl_setopt_array($curl, [
+                CURLOPT_RETURNTRANSFER => 1,
+            ]);
+            $res_body = curl_exec($curl);
+            $res_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+            curl_close($curl);
+
+            if($res_code == 200) {
+                $pokemons = json_decode($res_body);
+                return $pokemons->pokemon;
+            } return ["pokemon" => []];
         }
 
         $url = 'https://www.canalti.com.br/api/pokemons.json';
@@ -35,6 +34,36 @@
     <pre>
         <?php print_r($pokemons); ?>
     </pre>
+<!-- 
+    foreach($pokemons as $pokemon){
+        echo $pokemon->id;
+        echo $pokemon->num;
+        echo $pokemon->name;
+        echo $pokemon->img;
+        foreach($pokemon->type as $type){
+            echo $type;
+        }
+        echo $pokemon->height;
+        echo $pokemon->weight;
+        echo $pokemon->candy;
+        echo $pokemon->candy_count;
+        echo $pokemon->egg;
+        echo $pokemon->spawn_chance;
+        echo $pokemon->abg_spawns;
+        echo $pokemon->spawn_time;
+        foreach($pokemon->multipliers as $multipliers) {
+            echo $multipliers;
+        }
+        foreach($pokemon->weaknesses as $weakness) {
+            echo $weakness;
+        }
+        foreach($pokemon->next_evolution as $next_evolution) {
+            foreach($next_evolution as $next_evolution) {
+                echo $next_evolution->num;
+                echo $next_evolution->name;
+            }
+        }
+    } -->
 
 </body>
 </html>
